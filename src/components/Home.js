ddf84b89febfae707c916d6ea532f254bb0c1e73
Timothy Hoang <timh1203@gmail.com>
2018-10-27 15:30:06 -0400

    Add copy to Home component

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Home = () => {
+  return (
+    <Div1>
+      <H1a>Full Stack Developer</H1a>
+      <H2a>Lambda School Student</H2a>
+      <H3a>"A mind stretched by new expriences can never go back to its old dimensions."</H3a>
+    </Div1>
+  )
+}
+
+const Div1 = styled.div``
+const H1a = styled.h1``
+const H2a = styled.h2``
+const H3a = styled.h3``
+
+export default Home;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Nav from '../components/Nav.js'
+import Home from '../components/Home.js'
 
 const IndexPage = () => (
   <Div1>
     <Nav />
+    <Home />
   </Div1>
 )
 
